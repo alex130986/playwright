75e7a47eb9e97eadf9ac56687e832cbb491b15a9
Alex <at13091986@gmail.com>
2025-09-14 14:51:31 +0200

    config_files_change

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     screenshot:'only-on-failure',
-     headless: false,
+      headless: !!process.env.CI,
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
